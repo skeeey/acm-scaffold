30d84a798824292adc7bb16e33407a2922365d5a
Wei Liu <liuweixa@redhat.com>
2022-12-20 10:20:26 +0000

    update deploy acm ds script
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- downstream/acm-deploy.sh
+++ downstream/acm-deploy.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-deploy_repo="/home/centos/deploy"
+CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+DEMO_DIR="$(cd ${CURRENT_DIR} && pwd)"
 
-export DOCKER_CONFIG=$(cat docker.token)
-export QUAY_TOKEN=$(echo $DOCKER_CONFIG | base64 -d | sed "s/quay\.io/quay\.io:443/g" | base64 -w 0)
+oc apply -f image-registry/image-content-source-policy.yaml
 
-export COMPOSITE_BUNDLE=true
-export DOWNSTREAM=true
+echo "Wait for all nodes restarting"
+oc get nodes -w
 
-export CUSTOM_REGISTRY_REPO="quay.io:443/acm-d"
+auth_basic=$(cat auth_file)
+oc -n openshift-config get secret pull-secret --template='{{index .data ".dockerconfigjson" | base64decode}}' >pull_secret.yaml
+oc registry login --registry="quay.io:443" --auth-basic="$auth_basic" --to=pull_secret.yaml
+oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=pull_secret.yaml
+
+echo "Wait for all nodes restarting"
+kubectl get nodes -w
+
+deploy_repo="${HOME}/deploy"
 
 pushd ${deploy_repo}
-kubectl apply -k addons/downstream
+export DOCKER_CONFIG=$(cat ${DEMO_DIR}/docker.token)
+export QUAY_TOKEN=$(echo $DOCKER_CONFIG | base64 -d | sed "s/quay\.io/quay\.io:443/g" | base64 -w 0)
+export COMPOSITE_BUNDLE=true
+export DOWNSTREAM=true
+export CUSTOM_REGISTRY_REPO="quay.io:443/acm-d"
 ./start.sh --watch
 popd
 
