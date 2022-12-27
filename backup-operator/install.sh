6a0c1fecf268fd88f6b43851548d28b6b9887b29
Wei Liu <liuweixa@redhat.com>
2022-12-27 05:58:40 +0000

    backup-restore
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- backup-operator/install.sh
+++ backup-operator/install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+PARENT_DIR="$(cd ../${CURRENT_DIR} && pwd)"
+
+source "${PARENT_DIR}"/utils
 
 kubectl patch MultiClusterHub multiclusterhub -n open-cluster-management --type=json -p='[{"op": "add", "path": "/spec/overrides/components/-","value":{"name":"cluster-backup","enabled":true}}]'
 
-kubectl delete secret cloud-credentials --namespace open-cluster-management-backup --ignore-not-found
-kubectl create secret generic cloud-credentials --namespace open-cluster-management-backup --from-file cloud=/home/centos/.aws/default-credentials
+wait_command "kubectl -n open-cluster-management-backup get deploy openshift-adp-controller-manager"
+wait_command "kubectl -n open-cluster-management-backup get deploy cluster-backup-chart-clusterbackup"
+
+kubectl -n open-cluster-management-backup rollout status deploy openshift-adp-controller-manager --timeout=120s
+kubectl -n open-cluster-management-backup rollout status deploy cluster-backup-chart-clusterbackup --timeout=120s
+
+kubectl -n open-cluster-management-backup delete secret cloud-credentials --ignore-not-found
+kubectl -n open-cluster-management-backup create secret generic cloud-credentials --from-file cloud=${HOME}/.aws/default-credentials
 
 kubectl apply -f dpa.yaml
+
+kubectl -n open-cluster-management-backup get dataprotectionapplication.oadp.openshift.io/dpa -ojsonpath='{.status.conditions}' -w
