6c359fc7bcfc0458017487e6ba22893af4db3783
Wei Liu <liuweixa@redhat.com>
2023-04-21 05:27:34 +0000

    cleanup works
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- example/manifestworks/batch/cleanup.sh
+++ example/manifestworks/batch/cleanup.sh
@@ -6,4 +6,8 @@ if [ "$1"x = "force"x ]; then
     kubectl -n $clustername get manifestworks -l perftest.open-cluster-management.io/work | grep -v NAME | awk '{print $1}' | xargs kubectl -n $clustername patch manifestworks -p '{"metadata":{"finalizers": []}}' --type=merge
 fi
 
-kubectl -n $clustername delete manifestworks -l perftest.open-cluster-management.io/work
+counts=1000
+for((i=0;i<$counts;i++));
+do
+    time kubectl -n $clustername delete manifestworks perftest-${clustername}-work-${i}
+done
