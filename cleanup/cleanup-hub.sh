7678f33716b06180ae2b10c193e54fc3cb73fd1d
Wei Liu <liuweixa@redhat.com>
2023-09-19 11:10:07 +0000

    clean up obs
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- cleanup/cleanup-hub.sh
+++ cleanup/cleanup-hub.sh
@@ -11,9 +11,16 @@ kubectl delete crds observatoria.core.observatorium.io
 
 kubectl get crds | grep open-cluster-management | awk '{print $1}' | xargs kubectl delete crds
 
+kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io application-webhook-validator multicluster-observability-operator multiclusterengines.multicluster.openshift.io
+
 echo "##########  patch local-cluster"
 kubectl get roles -n local-cluster | grep -v NAME | awk '{print $1}' | xargs kubectl -n local-cluster patch roles -p '{"metadata":{"finalizers": []}}' --type=merge
 kubectl get rolebindings -n local-cluster | grep -v NAME | awk '{print $1}' | xargs kubectl -n local-cluster patch rolebindings -p '{"metadata":{"finalizers": []}}' --type=merge
 
 echo "########## patch managedclustersets"
 kubectl get managedclustersets | grep -v NAME | awk '{print $1}' | xargs kubectl patch managedclustersets -p '{"metadata":{"finalizers": []}}' --type=merge
+
+kubectl get multiclusterobservabilities | grep -v NAME | awk '{print $1}' | xargs kubectl patch multiclusterobservabilities -p '{"metadata":{"finalizers": []}}' --type=merge
+
+kubectl delete ns open-cluster-management-observability --ignore-not-found
+kubectl delete namespace hive --ignore-not-found
