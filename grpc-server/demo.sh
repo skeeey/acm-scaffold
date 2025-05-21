574579a6f6218ed9e3d66eb3e6e4a13d3d1da833
Wei Liu <liuweixa@redhat.com>
2025-05-21 15:29:51 +0800

    grpc server
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- grpc-server/demo.sh
+++ grpc-server/demo.sh
@@ -6,26 +6,27 @@ DEMO_DIR="$(cd ${CURRENT_DIR} && pwd)"
 source "${PARENT_DIR}"/demo_magic
 source "${PARENT_DIR}"/utils
 
-comment "A gRPC server is introduced on the hub, the managedcluster agents will transport its resources over this gRPC server"
+comment "A gRPC server is introduced on the hub, the klusterlet agents will transport resources over this gRPC server"
 pe "kubectl -n open-cluster-management-hub get pods,svc"
 
 # register cluster
-comment "Deploy managedcluster agents using gRPC bootstrap config on a spoke cluster to register the spoke cluster"
+comment "Deploy klusterlet agents using a gRPC bootstrap config on a spoke cluster to register the spoke cluster"
 pe "kubectl -n open-cluster-management-agent get secrets bootstrap-secret -ojsonpath='{.data.config\.yaml}' | base64 -d"
 pe "kubectl apply -k deploy/spoke"
 pe "kubectl -n open-cluster-management-agent get pods -w"
 
-comment "Get managedclusters on the hub"
+comment "When the klusterlet registration agent start up, it will create ManagedCluster and CSR on the hub"
 pe "kubectl get managedclusters -w"
 pe "kubectl get csr -w"
 
-comment "Approve the managedcluster"
+comment "Accept the managedcluster"
 pe "kubectl patch managedcluster cluster1 -p='{\"spec\":{\"hubAcceptsClient\":true}}' --type=merge"
 pe "kubectl get csr -l open-cluster-management.io/cluster-name=cluster1 | grep Pending | awk '{print \$1}' | xargs kubectl certificate approve"
 pe "kubectl get managedclusters -w"
-comment "After the managedcluster is registered, the work agent will connect to the hub over gRPC"
+comment "After the managedcluster is registered, a new hub gRPC config will be created on the managedcluster"
 pe "kubectl -n open-cluster-management-agent get secrets -w"
 pe "kubectl -n open-cluster-management-agent get secrets hub-kubeconfig-secret -ojsonpath='{.data.config\.yaml}' | base64 -d"
+comment "The work agent connects to the hub over gRPC with the new config"
 pe "kubectl -n open-cluster-management-agent get pods -w"
 
 # apply a manifestwork
